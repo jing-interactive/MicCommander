74098d5d4f008857c06332eeb0002067f951ba7a
vinjn <vinjn.z@gmail.com>
2017-08-30 23:23:43 +0800

    Connecting the dots.

--- src/MicCommanderApp.cpp
+++ src/MicCommanderApp.cpp
@@ -3,64 +3,141 @@
 #include "cinder/gl/gl.h"
 #include "cinder/CameraUi.h"
 #include "cinder/Log.h"
+#include "cinder/params/Params.h"
 
 #include "AssetManager.h"
 #include "MiniConfig.h"
+
+#include "msp_cmn.h"
+#include "msp_errors.h"
+#include "speech_recognizer.h"
+
+#define FRAME_LEN	640 
+#define	BUFFER_SIZE	4096
+
+const char* login_params = "appid = 57bb9136, work_dir = ."; // 登录参数，appid与msc库绑定,请勿随意改动
+/*
+* sub:				请求业务类型
+* domain:			领域
+* language:			语言
+* accent:			方言
+* sample_rate:		音频采样率
+* result_type:		识别结果格式
+* result_encoding:	结果编码格式
+*/
+const char* session_begin_params = "sub = iat, domain = iat, language = zh_cn, accent = mandarin, sample_rate = 16000, result_type = plain, result_encoding = gb2312";
 
 using namespace ci;
 using namespace ci::app;
 using namespace std;
 
+static char *g_result = NULL;
+static unsigned int g_buffersize = BUFFER_SIZE;
+
+void on_result(const char *result, char is_last)
+{
+    if (result) {
+        size_t left = g_buffersize - 1 - strlen(g_result);
+        size_t size = strlen(result);
+        if (left < size) {
+            g_result = (char*)realloc(g_result, g_buffersize + BUFFER_SIZE);
+            if (g_result)
+                g_buffersize += BUFFER_SIZE;
+            else {
+                CI_LOG_I("mem alloc failed");
+                return;
+            }
+        }
+        strncat(g_result, result, size);
+        _WORD = g_result;
+    }
+}
+void on_speech_begin()
+{
+    if (g_result)
+    {
+        free(g_result);
+    }
+    g_result = (char*)malloc(BUFFER_SIZE);
+    g_buffersize = BUFFER_SIZE;
+    memset(g_result, 0, g_buffersize);
+
+    _STATUS = "Start Listening...";
+}
+
+void on_speech_end(int reason)
+{
+    if (reason == END_REASON_VAD_DETECT)
+        _STATUS = "Speaking done";
+    else
+        _STATUS = "Recognizer error";
+}
+
 class MicCommanderApp : public App
 {
-  public:
+    speech_rec iat;
+
+public:
     void setup() override
     {
         log::makeLogger<log::LoggerFile>();
-        
-        auto aabb = am::triMesh(MESH_NAME)->calcBoundingBox();
-        mCam.lookAt(aabb.getMax() * 2.0f, aabb.getCenter());
-        mCamUi = CameraUi( &mCam, getWindow(), -1 );
-        
-        createConfigUI({200, 200});
-        gl::enableDepth();
-    
-        getWindow()->getSignalResize().connect([&] {
-            mCam.setAspectRatio( getWindowAspectRatio() );
+
+        int ret = MSPLogin(NULL, NULL, login_params); //第一个参数是用户名，第二个参数是密码，均传NULL即可，第三个参数是登录参数	
+        if (MSP_SUCCESS != ret) {
+            CI_LOG_E("MSPLogin failed , Error code " << ret);
+            quit();
+            return;
+        }
+
+        speech_rec_notifier recnotifier =
+        {
+            on_result,
+            on_speech_begin,
+            on_speech_end
+        };
+
+        int errcode = sr_init(&iat, session_begin_params, SR_MIC, DEFAULT_INPUT_DEVID, &recnotifier);
+        if (errcode) {
+            CI_LOG_E("speech recognizer init failed\n");
+            quit();
+            return;
+        }
+
+        auto params = createConfigUI({ 400, 200 });
+        params->addButton("Start Record", [&] {
+            if (int errcode = sr_start_listening(&iat)) {
+                _STATUS = "Start listen failed";
+                //quit();
+            }
+        });
+
+        params->addButton("Stop Record", [&] {
+            if (int errcode = sr_stop_listening(&iat)) {
+                _STATUS = "Stop listen failed";
+                //quit();
+            }
         });
 
         getWindow()->getSignalKeyUp().connect([&](KeyEvent& event) {
             if (event.getCode() == KeyEvent::KEY_ESCAPE) quit();
         });
-        
-        mGlslProg = am::glslProg(VS_NAME, FS_NAME);
-        mGlslProg->uniform("uTex0", 0);
-        mGlslProg->uniform("uTex1", 1);
-        mGlslProg->uniform("uTex2", 2);
-        mGlslProg->uniform("uTex3", 3);
 
         getWindow()->getSignalDraw().connect([&] {
-            gl::setMatrices( mCam );
             gl::clear();
-        
-            gl::ScopedTextureBind tex0(am::texture2d(TEX0_NAME), 0);
-            gl::ScopedTextureBind tex1(am::texture2d(TEX1_NAME), 1);
-            gl::ScopedTextureBind tex2(am::texture2d(TEX2_NAME), 2);
-            gl::ScopedTextureBind tex3(am::texture2d(TEX3_NAME), 3);
-            gl::ScopedGlslProg glsl(mGlslProg);
+            gl::ScopedGlslProg glsl(am::glslProg("color"));
+        });
 
-            gl::draw(am::vboMesh(MESH_NAME));
+        getSignalCleanup().connect([&] {
+            sr_stop_listening(&iat);
+            sr_uninit(&iat);
+            MSPLogout();
         });
     }
-    
-private:
-    CameraPersp         mCam;
-    CameraUi            mCamUi;
-    gl::GlslProgRef     mGlslProg;
 };
 
-CINDER_APP( MicCommanderApp, RendererGl, [](App::Settings* settings) {
+CINDER_APP(MicCommanderApp, RendererGl, [](App::Settings* settings) {
     readConfig();
-    settings->setWindowSize(APP_WIDTH, APP_HEIGHT);
+    _STATUS = "Waiting for your input.";
+    settings->setWindowSize(_APP_WIDTH, _APP_HEIGHT);
     settings->setMultiTouchEnabled(false);
-} )
+})
